20bf7e8968e526673b55d9df57f3a10dbc0e9bfc
Corey Shanahan <coreyshanahan01@gmail.com>
2022-11-30 13:32:22 +0000

    added a actor header

--- src/components/templateActorDetails/index.js
+++ src/components/templateActorDetails/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import { CardMedia } from "@mui/material";
+import Header from "../headerActor";
 
 const TemplateActorPage = ({ actor, children }) => { 
     const img = `https://image.tmdb.org/t/p/w500/${actor.profile_path}`;
   return (
     <>
+      <Header actor={actor}></Header>
       <Grid container spacing={5} sx={{ padding: "15px" }}>
         <Grid item xs={3}>
           <div sx={{
